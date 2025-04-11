7b645fee0e6f1d29df541bb1be66974b4bd62715
seifu <s>
2025-04-11 10:36:45 +0300

    изучение синтаксиса, и методов  C#

---
+++ BlazorApp1/Models/Carsmodel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorApp1.Models
+{
+
+    public class Carsmodel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+        public string? Make { get; set; }
+        public string? Model { get; set; }
+        public double? Year { get; set; }
+        public string? Price { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Engine { get; set; }
+        public string? Transmission { get; set; }
+        public string? Drive { get; set; }
+        public string? Meleage { get; set; }
+        public string? Power { get; set; }
+    }
+}
---
+++ BlazorApp1/SeifDbContext.cs
@@ -0,0 +1,13 @@
+﻿
+using BlazorApp1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp1
+{
+    public class SeifDbContext : DbContext 
+    {
+        public DbSet<Carsmodel>  Carsmodel { get; set; }
+        public DbSet<Users> Users {  get; set; }
+        public SeifDbContext(DbContextOptions<SeifDbContext> options):base(options) { }
+    }
+}
