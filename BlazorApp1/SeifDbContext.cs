8f0151c1bfcb802ec65d632ec18863e8fc9af1f5
seifu <s>
2025-06-28 13:11:18 +0300

    changes

--- BlazorApp1/SeifDbContext.cs
+++ BlazorApp1/SeifDbContext.cs
@@ -4,10 +4,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BlazorApp1
 {
-    public class SeifDbContext : DbContext 
+    public class SeifDbContext : DbContext                                                  
     {
-        public DbSet<Carsmodel>  Carsmodel { get; set; }
-        public DbSet<Users> Users {  get; set; }
+        public DbSet<Carsmodel>?  Carsmodel { get; set; }
+        public DbSet<Users>? Users {  get; set; }
         public SeifDbContext(DbContextOptions<SeifDbContext> options):base(options) { }
     }
 }
